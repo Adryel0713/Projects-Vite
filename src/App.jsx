20beee5536a88278f33d241e22a2082b646f13ae
Adryel0713 <clauber.adriel@gmail.com>
2025-10-20 05:38:22 -0300

    Project (7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import Counter from "./Components/Counter"
 // import Todo from "./Components/Todo.jsx";
 
-import Testimonials from "./Components/Testimonials"
+// import Testimonials from "./Components/Testimonials"
 
 // import HiddenSearchBar from "./Components/HiddenSearchBar"
 
@@ -9,6 +9,9 @@ import Testimonials from "./Components/Testimonials"
 
 // import Calculator from "./Components/Calculator"
 // import Meals from "./Components/Meals"
+
+import Accordion from "./Components/Accordion"
+import { accordionData } from "./util/content"
 
 const App = () => {
   return (
@@ -19,7 +22,13 @@ const App = () => {
       {/* <Calculator /> */}
       {/* <ToggleBackgroundColor /> */}
       {/* <HiddenSearchBar /> */}
-      <Testimonials />
+      {/* <Testimonials /> */}
+
+      <div className="accordion">
+        {accordionData.map(({title,content}) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/util/content.js
@@ -0,0 +1,18 @@
+export const accordionData = [
+  {
+    title: "What is HTML??",
+    content: "It's the standard language used to create and structure web pages. HTML tells a web browser how to display text, images, links, videos, and other content on a webpage."
+  },
+  {
+    title: "What is React??",
+    content: "React is a JavaScript library used to build user interfaces (UIs) — especially for web applications."
+  },
+  {
+    title: "What is Node.JS??",
+    content: "Node.js is a runtime environment that lets you run JavaScript outside the browser — for example, on your computer or a server."
+  },
+  {
+    title: "What is Golang??",
+    content: "Golang, also called Go, is a programming language created by Google in 2009. It's known for being fast, simple, and very efficient, especially for building servers, network tools, APIs, and cloud-based applications."
+  },
+];
